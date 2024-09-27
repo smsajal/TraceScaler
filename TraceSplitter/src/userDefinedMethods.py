81ee193d600728758feeeaed97e3d13466ee3584
Sultan Mahmud Sajal <sxs2561@psu.edu>
2024-09-27 11:10:43 -0400

    redoing the repo from scratch

--- TraceSplitter/src/userDefinedMethods.py
+++
@@ -1,5 +0,0 @@
-
-
-# Converts request size based on request parameters into some size based on server speed. Formula given by user
-def reqSizeEstimation(requestSize, details):
-    return requestSize
--- TraceUpscaler/test/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar TraceUpscaler.jar source_trace.txt dest_trace.txt 2
